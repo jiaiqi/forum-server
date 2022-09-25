c6806c6004cf14d690c7bb9a02d6ffdccd773c63
jiaiqi <2468260248@qq.com>
2022-09-25 11:14:30 +0800

    ✨

--- src/modules/content/content.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-
-// src/modules/content/content.module.ts
-@Module({})
-export class ContentModule {}
--- src/modules/content/repositories/category.repository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, TreeRepository } from 'typeorm';
-import { CategoryEntity } from '../entities/category.entity';
-
-// src/modules/content/repositories/category.repository.ts
-@EntityRepository(CategoryEntity)
-export class CategoryRepository extends TreeRepository<CategoryEntity> {}
